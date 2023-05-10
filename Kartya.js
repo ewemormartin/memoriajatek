2ba681ab1e155529b2e48e1265b47a7391bceede
Ewemor Martin (SZF_2022_2024) <ewemor.martin@diak.szamalk-szalezi.hu>
2023-05-10 09:31:40 +0200

    orai

---
+++ Kartya.js
@@ -0,0 +1,17 @@
+class Kartya{
+    constructor (kep, id , szuloElem){
+        this.#kep=kep;
+        this.#id=id;
+        this.#szuloElem=szuloElem;
+    }
+    getKep(){
+
+    }
+    getId(){
+
+    }
+    setkepElem(kep){
+        this.
+
+    }
+}
