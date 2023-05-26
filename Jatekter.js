aaae823387484aebdcfb40c7e9963dd5215abfdd
Ewemor Martin <martinewemor@gmail.com>
2023-05-26 12:36:11 +0200

    órai

---
+++ Jatekter.js
@@ -0,0 +1,61 @@
+import kartya from "./Kartya.js";
+
+$(function(){
+    let szuloElem = $(".tarolo")
+        
+        for (let index = 0; index < LISTA.length; index++) {
+            var kepINdex = LISTA[index][20];
+            let k1 = new kartya(LISTA[index], kepINdex, szuloElem);
+            
+        }
+        
+});
+class JatekTer{
+
+
+
+kattint() {
+    let self = this;
+    let id = this.#id;
+    this.elem.on("click", function () {
+      KIVALASZTL.push(id);
+      console.log(id);
+
+      if (KIVALASZTL.length == 2) {
+        console.log(KIVALASZTL);
+      }
+
+      self.fordit(self.#kep);
+      setTimeout(function () {
+        self.visszafordit();
+      }, 300);
+      self.kivalasztas();
+      if (this.kapcs) {
+        self.kivalasztas();
+      }
+    });
+  }
+
+  fordit(kep) {
+    this.elem.find("img").attr("src", kep);
+  }
+  visszafordit() {
+    this.elem.find("img").attr("src", "./kepek/hatter.jpg");
+  }
+  kivalasztas() {
+    if (KIVALASZTL.length == 2) {
+      console.log("k1: ", KIVALASZTL[1]);
+      console.log("k2: ", KIVALASZTL[0]);
+      console.log("teljes: ", KIVALASZTL);
+  
+      if (KIVALASZTL[1] === KIVALASZTL[0]) {
+        this.elem.find(`img#${KIVALASZTL[1]}`).css("visibility", "hidden");
+        this.elem.find(`img#${KIVALASZTL[0]}`).css("visibility", "hidden");
+      } else {
+        this.visszafordit();
+      }
+      KIVALASZTL = [];
+    }
+  }
+}
+  export default JatekTer;
--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-import kartya from "./Kartya.js";
+import JatekTer from "./Jatekter.js";
 import {LISTA} from "./adatok.js";
 
 $(function(){
-    let szuloElem = $(".tarolo")
-    
-    for (let index = 0; index < LISTA.length; index++) {
-        var kepINdex = LISTA[index][20];
-        let k1 = new kartya(LISTA[index], kepINdex, szuloElem);
-        
-    }
-    
-});
+    new JatekTer(LISTA);
+})
